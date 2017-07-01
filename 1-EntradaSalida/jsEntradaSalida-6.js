198534b981eaa6db72542f1ff066d458f77ad33e
MunozAle12 <munozalito@gmail.com>
2017-06-30 22:17:02 -0300

    Commit 3

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,13 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	var NumUno= document.getElementById('numeroUno').value;
+	var NumDos= document.getElementById('numeroDos').value;
+    
+    NumUno= parseInt(NumUno);
+    NumDos= parseInt(NumDos);
+    
+    var resultado= (NumUno+NumDos);
+    alert(resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,49 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var NumUno= document.getElementById('numeroUno').value;
+	var NumDos= document.getElementById('numeroDos').value;
+    
+    NumUno= parseInt(NumUno);
+    NumDos= parseInt(NumDos);
+    
+    var resultado= (NumUno+NumDos);
+    alert("La suma es " +resultado);
 }
 
 function restar()
 {
-	
+	var NumUno= document.getElementById('numeroUno').value;
+	var NumDos= document.getElementById('numeroDos').value;
+    
+    NumUno= parseInt(NumUno);
+    NumDos= parseInt(NumDos);
+    
+    var resultado= (NumUno-NumDos);
+    alert("La resta es " +resultado);
 }
 
 function multiplicar()
 { 
-	
+	var NumUno= document.getElementById('numeroUno').value;
+	var NumDos= document.getElementById('numeroDos').value;
+    
+    NumUno= parseInt(NumUno);
+    NumDos= parseInt(NumDos);
+    
+    var resultado= (NumUno*NumDos);
+    alert("La multiplicación es " +resultado);
 }
 
 function dividir()
 {
-	
+	var NumUno= document.getElementById('numeroUno').value;
+	var NumDos= document.getElementById('numeroDos').value;
+    
+    NumUno= parseInt(NumUno);
+    NumDos= parseInt(NumDos);
+    
+    var resultado= (NumUno/NumDos);
+    alert("La división es " +resultado);
 }
 
