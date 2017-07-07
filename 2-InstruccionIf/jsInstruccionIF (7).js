bc37111ee4a800844e499ffb43b6d319218cc05d
MunozAle12 <munozalito@gmail.com>
2017-07-07 15:58:24 -0300

    Sentencias de control de flujo.
    Sentencias if, if-else, if-else-if.

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,9 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad= document.getElementById('edad').value;
+var estadoCivil= document.getElementById('estadoCivil').value;
 
-	
 
 
 }//FIN DE LA FUNCIÓN
