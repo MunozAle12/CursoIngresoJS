59bfc4786de46620452f954ee4c46ef20d8a6185
MunozAle12 <munozalito@gmail.com>
2017-07-09 00:40:33 -0300

    Sentencias If, If-else.

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,9 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
-var edad= document.getElementById('edad').value;
-var estadoCivil= document.getElementById('estadoCivil').value;
-	if(edad<18&&estadoCivil!=estadoCivil)
+var edad;
+var estadoCivil;
+
+edad = document.getElementById('edad').value;
+estadoCivil = document.getElementById('estadoCivil').value;
+	
+	if(edad<18 && estadoCivil!="Soltero")
 	{
 		alert("Es muy pequeño para NO ser soltero");
 	}
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,10 +1,15 @@
 function Mostrar()
 {
-//tomo la edad  
-var edad= document.getElementById('edad').value;
-var estadoCivil= document.getElementById('estadoCivil').value;
-	if(edad<17&&estadoCivil=estadoCivil)
+//tomo la edad
+var edad;
+var estadoCivil;
+
+edad = document.getElementById('edad').value;
+estadoCivil = document.getElementById('estadoCivil').value;
+	
+	if(edad>17 && estadoCivil="Soltero")
 	{
 		alert("Es soltero y no es menor de edad");
 	}
+	
 }//FIN DE LA FUNCIÓN
